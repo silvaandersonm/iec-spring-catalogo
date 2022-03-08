8e552a9ca955b6d195104e0f96c8f36ddb117e62
Anderson Machado da Silva <silva.andersonm@gmail.com>
2022-03-08 12:40:28 -0300

    Atualizado o mapeamento das tabelas.

--- src/main/java/pro/gsilva/catalogo/model/Categoria.java
+++ src/main/java/pro/gsilva/catalogo/model/Categoria.java
@@ -2,16 +2,26 @@ package pro.gsilva.catalogo.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 @Entity
+@Table(name="categoria")
 @Data
 public class Categoria {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id_categoria")
     private Long id;
+
+    @Column(name="nm_categoria", length=30, nullable=false)
+    @NotBlank
     private String nome;
+
 }
--- src/main/java/pro/gsilva/catalogo/model/Musica.java
+++ src/main/java/pro/gsilva/catalogo/model/Musica.java
@@ -1,13 +1,15 @@
 package pro.gsilva.catalogo.model;
 
-
 import java.time.LocalDate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
@@ -16,26 +18,34 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 @Entity
-@Table(name = "TB_MUSICA")
+@Table(name="musica")
 @Data
 public class Musica {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id_musica")
     private Long id;
 
-    @NotBlank
-    private String autor;
-
+    @Column(name="ds_titulo", length=60, nullable=false)
     @NotBlank
     private String titulo;
 
+    @Column(name="nm_autor", length=60, nullable=false)
+    @NotBlank
+    private String autor;
+
+    @Column(name="dt_lancamento", nullable=false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss", locale = "UTC-03")
     private LocalDate data;
 
+    @Column(name="ds_letra", nullable=false)
     @NotBlank
     @Lob
     private String letra;
 
-    
+    @ManyToOne
+	@JoinColumn(name="id_categoria", nullable=false)
+	private Categoria categoria;
+
 }
